bcf15719ad93221025ae0bde96f8e12e7af736a9
aman <a.kasa@alustudent.com>
2024-10-29 03:51:21 -0700

    ads

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,21 @@
-# This Puppet manifest configures the SSH client configuration file
+# This Puppet manifest configures the global SSH client configuration file
 
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',  # Set file permissions to read/write for owner only
-  content => template('ssh/config.erb'),  # Use a template for dynamic content
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',  # Set file permissions to read/write for owner and read for others
+  content => template('ssh/ssh_config.erb'),  # Using a template for the content
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => 'PasswordAuthentication yes',  # Only add if it exists
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => 'IdentityFile',
 }
