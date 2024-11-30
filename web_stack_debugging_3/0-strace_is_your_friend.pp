e08e609f2683131be01208b0a0004e8176c03746
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 20:48:39 +0000

    zx trace

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,6 @@
-# 0-strace_is_your_friend.pp
-# This manifest fixes Apache's 500 error by addressing a configuration issue.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Ensure the Apache configuration file is present with the correct content.
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => present,
-  content => '
-<VirtualHost *:80>
-    DocumentRoot /var/www/html
-    # You can add more configuration details here as needed
-</VirtualHost>',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
-# Ensure the Apache service is running and enabled.
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/sites-available/000-default.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
