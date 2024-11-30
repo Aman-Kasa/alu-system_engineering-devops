68d1f41a7ba01e325c6ff19cd47dfc597454c137
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 20:51:41 +0000

    wordpress debugger

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,30 +1,6 @@
-# This Puppet manifest optimizes Nginx to handle high concurrency and avoid failed requests
+# fix nginx to accept and serve more requests
 
-# Ensure the Nginx package is installed
-package { 'nginx':
-  ensure => installed,
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-# Ensure the Nginx configuration file has the correct settings for high concurrency
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/optimized_nginx.conf'),
-  notify  => Service['nginx'],  # Ensure nginx is reloaded when the config changes
-}
-
-# Ensure Nginx is running and enabled at boot
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/nginx.conf'],  # Reload service when config changes
-}
-
-# Ensure the system file descriptor limit is increased
-exec { 'increase-file-descriptors':
-  command => '/bin/ulimit -n 65536',
-  unless  => 'ulimit -n | grep -q 65536',
-  require => Package['nginx'],
-}
-
-# Ensure the system resources (like CPU/memory) are sufficient (you may need to adjust manually)
-# Puppet can't directly manage CPU or memory, but ensure monitoring and alerts are set up
