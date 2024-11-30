0355e8965229faa14e33ec820af587f4b90d1245
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 20:45:22 +0000

    x

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,22 @@
-exec { 'fix-wp-locale-path':
-  command => '/bin/sed -i "s|/wp-includes/class-wp-locale.phpp|' +
-             '/wp-includes/class-wp-locale.php|g" ' +
-             '/var/www/html/wp-config.php',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => '/bin/grep -q "class-wp-locale.phpp" ' +
-             '/var/www/html/wp-config.php',
+# 0-strace_is_your_friend.pp
+# This manifest fixes Apache's 500 error by addressing a configuration issue.
+
+# Ensure the Apache configuration file is present with the correct content.
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => present,
+  content => '
+<VirtualHost *:80>
+    DocumentRoot /var/www/html
+    # You can add more configuration details here as needed
+</VirtualHost>',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
 }
 
+# Ensure the Apache service is running and enabled.
 service { 'apache2':
   ensure    => running,
   enable    => true,
-  subscribe => Exec['fix-wp-locale-path'],
+  subscribe => File['/etc/apache2/sites-available/000-default.conf'],
 }
