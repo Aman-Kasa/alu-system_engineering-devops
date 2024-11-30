564ee2d0d642d77cf2861bec8d3e260d27f8f7c8
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 20:31:30 +0000

    c

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,30 +1,14 @@
-# Puppet manifest to resolve WordPress 500 Internal Server Error
-
-# Specific fix for WordPress configuration
-exec { 'fix-wordpress-configuration':
-        command => '/bin/sed -i "s/MyISAM/InnoDB/g" /var/www/html/wp-config.php',
-        path    => ['/bin', '/usr/bin'],
-        onlyif  => '/bin/grep -q "MyISAM" /var/www/html/wp-config.php',
-}
-
-file { '/var/www/html/wp-config.php':
-        ensure => file,
-        mode   => '0644',
-        owner  => 'www-data',
-        group  => 'www-data',
-}
-
-exec { 'fix-wordpress-permissions':
-        command => '/bin/chmod -R 755 /var/www/html',
-        path    => ['/bin', '/usr/bin'],
+exec { 'fix-wp-locale-path':
+  command => '/bin/sed -i "s|/wp-includes/class-wp-locale.phpp|' +
+             '/wp-includes/class-wp-locale.php|g" ' +
+             '/var/www/html/wp-config.php',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => '/bin/grep -q "class-wp-locale.phpp" ' +
+             '/var/www/html/wp-config.php',
 }
 
 service { 'apache2':
-        ensure    => running,
-        enable    => true,
-        subscribe => [
-                File['/var/www/html/wp-config.php'],
-                Exec['fix-wordpress-configuration'],
-                Exec['fix-wordpress-permissions']
-        ],
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['fix-wp-locale-path'],
 }
