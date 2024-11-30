6b3f2572487a56a3e6f6540085a2cd0b145ebf25
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 19:15:07 +0000

    debug 3

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,29 @@
+# Puppet manifest to resolve WordPress 500 Internal Server Error
+
+# Ensure wp-config.php has correct permissions
+file { '/var/www/html/wp-config.php':
+  ensure  => file,
+  mode    => '0644',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
+# Fix WordPress directory permissions
+exec { 'fix-wordpress-permissions':
+  command => '/bin/chmod -R 755 /var/www/html',
+  path    => ['/bin', '/usr/bin'],
+}
+
+# Ensure PHP MySQL extension is installed
+package { 'php5-mysql':
+  ensure => installed,
+}
+
+# Ensure proper Apache configuration
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => [
+    File['/var/www/html/wp-config.php'],
+    Exec['fix-wordpress-permissions']
+  ],}
