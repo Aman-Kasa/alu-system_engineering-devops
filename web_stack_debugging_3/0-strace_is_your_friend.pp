4cae67d5499640570e2acfb01398919eaf0a7fa7
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 19:25:56 +0000

    Resolve WordPress 500 error with Puppet manifest

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,29 +2,29 @@
 
 # Specific fix for WordPress configuration
 exec { 'fix-wordpress-configuration':
-    command => '/bin/sed -i "s/MyISAM/InnoDB/g" /var/www/html/wp-config.php',
-    path    => ['/bin', '/usr/bin'],
-    onlyif  => '/bin/grep -q "MyISAM" /var/www/html/wp-config.php',
+        command => '/bin/sed -i "s/MyISAM/InnoDB/g" /var/www/html/wp-config.php',
+        path    => ['/bin', '/usr/bin'],
+        onlyif  => '/bin/grep -q "MyISAM" /var/www/html/wp-config.php',
 }
 
 file { '/var/www/html/wp-config.php':
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'www-data',
-    group   => 'www-data',
+        ensure => file,
+        mode   => '0644',
+        owner  => 'www-data',
+        group  => 'www-data',
 }
 
 exec { 'fix-wordpress-permissions':
-    command => '/bin/chmod -R 755 /var/www/html',
-    path    => ['/bin', '/usr/bin'],
+        command => '/bin/chmod -R 755 /var/www/html',
+        path    => ['/bin', '/usr/bin'],
 }
 
 service { 'apache2':
-    ensure    => running,
-    enable    => true,
-    subscribe => [
-        Exec['fix-wordpress-configuration'],
-        File['/var/www/html/wp-config.php'],
-        Exec['fix-wordpress-permissions']
-    ],
+        ensure    => running,
+        enable    => true,
+        subscribe => [
+                File['/var/www/html/wp-config.php'],
+                Exec['fix-wordpress-configuration'],
+                Exec['fix-wordpress-permissions']
+        ],
 }
