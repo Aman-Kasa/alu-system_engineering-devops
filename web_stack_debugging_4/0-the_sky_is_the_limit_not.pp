e82ab633ed592114f22da25d3f86b030ba00abd5
Aman-Kasa <a.kasa@alustudent.com>
2024-11-30 17:04:59 +0000

    debugged indentation on ==>

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -11,38 +11,38 @@ worker_processes auto;
 pid /run/nginx.pid;
 
 events {
-    worker_connections 4096;
+  worker_connections 4096;
 }
 
 http {
-    sendfile on;
-    tcp_nopush on;
-    tcp_nodelay on;
-    keepalive_timeout 65;
-    types_hash_max_size 2048;
+  sendfile on;
+  tcp_nopush on;
+  tcp_nodelay on;
+  keepalive_timeout 65;
+  types_hash_max_size 2048;
 
-    include /etc/nginx/mime.types;
-    default_type application/octet-stream;
+  include /etc/nginx/mime.types;
+  default_type application/octet-stream;
 
-    client_body_buffer_size 16K;
-    client_max_body_size 32M;
-    client_header_buffer_size 8k;
-    large_client_header_buffers 4 16k;
+  client_body_buffer_size 16K;
+  client_max_body_size 32M;
+  client_header_buffer_size 8k;
+  large_client_header_buffers 4 16k;
 
-    gzip on;
-    gzip_disable \"msie6\";
+  gzip on;
+  gzip_disable \"msie6\";
 
-    include /etc/nginx/conf.d/*.conf;
-    include /etc/nginx/sites-enabled/*;
+  include /etc/nginx/conf.d/*.conf;
+  include /etc/nginx/sites-enabled/*;
 }
-EOF' ",
+EOF'",
   onlyif  => '/bin/grep -q "worker_connections 4096;" /etc/nginx/nginx.conf',
   require => Package['nginx'],  # Ensure nginx is installed before this resource runs
 }
 
 # Ensure nginx service is running
 service { 'nginx':
-   ensure   => 'running',
-   enable   => true,
+  ensure    => 'running',
+  enable    => true,
   subscribe => Exec['optimize_nginx'],  # Restart nginx if exec changes its config
 }
